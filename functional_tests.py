68a452f2f46e84da4805621429f898b326495f96
nlawrence22 <nlawrence22@users.noreply.github.com>
2016-09-03 14:39:02 -0700

    Expand the functional test

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.common.exceptions import WebDriverException
+from selenium.webdriver.common.keys import Keys
 import unittest
 
 # The default firefoxDriver no longer plays nice with FF 48+ so we need to use
@@ -31,19 +32,34 @@ class NewVisitorTest(unittest.TestCase):
         except WebDriverException as e:
             print("WebDriverException encountered: {}".format(e))
         
-        # She notices the homepage has To-Do in the title
+        # She notices the homepage and header has To-Do in the title
         self.assertIn('To-Do', self.browser.title)
-        self.fail('Finish the test!')
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertEqual(
+                inputbox.get_attribute('placeholder'),
+                'Enter a to-do item'
+        )
 
         # She types "Buy groceries" into a text box
+        inputbox.send_keys('Buy groceries')
 
         # When she hits enter, the page updates, and now the page lists
         # "1: Buy groceries" as an item in a to-do list
+        inputbox.send_keys(Keys.ENTER)
+
+        table = self.browser.find_element_by_id('id_list_table')
+        rows = table.find_elements_by_tag_name('tr')
+        self.assertTrue(
+            any(row.text == '1: Buy groceries' for row in rows)
+        )
 
         # There is still a text box inviting her to add another item. She
         # enters "Cook Dinner"
+        self.fail('Finish the test!')
 
         # The page updates again, and now shows both items on her list
 
